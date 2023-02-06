6222b264fa80355b26fecd8001359805d3fca756
abbmarand <markus.andersson@abbgymnasiet.se>
2023-02-06 08:46:20 +0100

    reverted changes but kept migration

---
+++ prism-server/prisma/migrations/20230203094559_init/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Made the column `username` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "username" SET NOT NULL,
+ALTER COLUMN "password" SET NOT NULL;
---
+++ prism-server/prisma/migrations/20230206073035_init/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "username",
+ADD COLUMN     "name" TEXT,
+ALTER COLUMN "password" DROP NOT NULL;

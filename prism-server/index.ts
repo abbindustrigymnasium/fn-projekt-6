b5d3b55b8f6673b53f9ec0c6ea8b8d9fa0e3dae1
abbmarand <markus.andersson@abbgymnasiet.se>
2023-02-06 10:14:13 +0100

    sign in and sign up now works

--- prism-server/index.ts
+++ prism-server/index.ts
@@ -1,38 +1,80 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
 
-async function main () {
-    await prisma.user.create({
+app.use(cors())
+app.use(express.json())
 
-        data: {
+const PORT = process.env.PORT || 3000
 
-            username: 'Markus Andersson',
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
-            email: 'markus.andersson@abbgymnasiet.se',
-
-            password: 'abc',
-
-        },
-
-    })
-}
+app.get('/', (req: any, res: any) => {
+    res.send('hello world')
+})
 
 
-main()
+app.post("/login", async (req: any, res: any) => {
+    try {
+        const { email, password } = req.body
+        const exists = !!await prisma.user.findFirst(
+            {
+                where: {
+                    email: email,
+                    password: password
+                }
+            }
+        )
+        if (exists === true) {
+            res.send({ "login": true })
+        }
+        else {
+            res.send({ "login": false })
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+})
 
-    .then(async () => {
+// send data like this: {"email": "your email", "password": "your password" }
+//returns either 
+app.post("/signup", async (req: any, res: any) => {
+    try {
+        const { email, password } = req.body
+        const exists = !!await prisma.user.findFirst(
+            {
+                where: {
+                    email: email,
+                }
+            }
+        )
+        if (exists === true) {
+            res.send({ "newuser": { email }, "successful": false, "message": "account with email alredy exists" })
+            return
+        }
+        const user = await prisma.user.create({
 
-        await prisma.$disconnect()
+            data: {
 
-    })
+                email: email,
 
-    .catch(async (e) => {
+                password: password,
 
-        console.error(e)
+            },
 
-        await prisma.$disconnect()
+        })
+        res.json({ "newuser": { email }, "successful": true, "message": "new account created" })
 
-        process.exit(1)
-
-    })
+        //const users = await prisma.user.findMany()
+        //res.json(users)
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    }
+})

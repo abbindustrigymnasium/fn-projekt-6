d3250bc7d4d87a1187f77b3472769859fa801b40
abbmarand <markus.andersson@abbgymnasiet.se>
2023-02-07 14:16:37 +0100

    new migration

--- prism-server/index.ts
+++ prism-server/index.ts
@@ -113,13 +113,39 @@ app.post("/getuserbyid", async (req: any, res: any) => {
 
 })
 app.post("/addbalance", async (req: any, res: any) => {
+    const addbalance = req.body.balance
+    const addid = req.body.id
+    const user = await prisma.user.findFirst(
+        {
+            where: {
+                id: addid
+            }
+        }
+    )
+    const initbalance = user
+    const data = await prisma.user.update({
+        where: {
+            id: addid
+        },
+        data: {
+            //?
+        }
+    })
+
 })
 
 app.post("/withdraw", async (req: any, res: any) => {
 })
 
+//works but prisma says error
 app.get("/leaderboards", async (req: any, res: any) => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+        orderBy: [
+            {
+                donated: 'desc',
+            },
+        ],
+    })
     res.send(users)
 })
 

138b844eb5050a29a3f70c6090b3efa1731a08cd
abboliott <oliver.ottosson@abbgymnasiet.se>
2023-02-09 12:19:25 +0100

    Bilder och text tillagda till alla boxar

--- GambleForGood/app.vue
+++ GambleForGood/app.vue
@@ -11,26 +11,20 @@
     <div class="flex flex-wrap -m-4">
       <div class="xl:w-1/3 md:w-1/2 p-4">
         <div class="bg-white p-6 rounded-lg">
-          <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/r/roulette-switch/hero" alt="Image Size 720x400">
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="lg:h-60 xl:h-80 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/r/roulette-switch/hero" alt="Image Size 720x400">
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Roulette</h2>
         </div>
       </div>
       <div class="xl:w-1/3 md:w-1/2 p-4">
         <div class="bg-white p-6 rounded-lg">
-          <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://asset.kompas.com/crops/Pk_pN6vllxXy1RshYsEv74Q1BYA=/56x0:1553x998/750x500/data/photo/2021/06/16/60c8f9d68ff4a.jpg" alt="Image Size 720x400">
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="lg:h-60 xl:h-80 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://www.mpl.live/blog/wp-content/uploads/2022/02/Black-Jack.png" alt="Image Size 720x400">
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Black Jack</h2>
         </div>
       </div>
       <div class="xl:w-1/3 md:w-1/2 p-4">
         <div class="bg-white p-6 rounded-lg">
-          <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://images.immediate.co.uk/production/volatile/sites/7/2019/07/33-GettyImages-154260931-216706f.jpg?quality=90&resize=768%2C574" alt="Image Size 720x400">
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="lg:h-60 xl:h-80 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://en.uoldown.com/icon/android/online-casino-crash-gaming/online-casino-crash-gaming-android.png" alt="Image Size 720x400">
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Crash(?)</h2>
         </div>
       </div>
       <div class="xl:w-1/3 md:w-1/2 p-4">

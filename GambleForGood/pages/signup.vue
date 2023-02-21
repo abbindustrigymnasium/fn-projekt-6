1020980875f7446a219eb19f12e0d006ea4cc21b
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2023-02-21 21:07:06 +0100

    fix signup username ui and  redirect to login

--- GambleForGood/pages/signup.vue
+++ GambleForGood/pages/signup.vue
@@ -17,18 +17,11 @@
                 </div>
                 <div>
                     <div class="flex -mx-3">
-                        <div class="w-1/2 px-3 mb-5">
-                            <label for="" class="text-xs font-semibold px-1">First name</label>
+                        <div class="w-full px-3 mb-5">
+                            <label for="" class="text-xs font-semibold px-1">Username</label>
                             <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John">
-                            </div>
-                        </div>
-                        <div class="w-1/2 px-3 mb-5">
-                            <label for="" class="text-xs font-semibold px-1">Last name</label>
-                            <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                <input v-model="_username" type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="username">
                             </div>
                         </div>
                     </div>
@@ -52,7 +45,7 @@
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <button @click="getData" class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
+                            <button @click="signup" class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
                         </div>
                     </div>
                 </div>
@@ -74,8 +67,9 @@ definePageMeta({
 
 const _email = ref();
 const _password = ref();
+const _username = ref();
 
-async function getData() {
+async function signup() {
     try {
 
       axios.post(`${endpoint}/signup`, {
@@ -91,6 +85,8 @@ async function getData() {
 
     console.log(_email.value)
     console.log(_password.value)
+
+    await navigateTo('/login')
   
     } catch (error) {
       console.error(error);

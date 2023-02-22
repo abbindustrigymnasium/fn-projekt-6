1c96de8c050d7dae1f503915946c38d6c0c6bbec
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2023-02-22 22:15:18 +0100

    +continue from organizations

--- GambleForGood/pages/organizations.vue
+++ GambleForGood/pages/organizations.vue
@@ -696,9 +696,28 @@
          </div>
          
       </div>
+      <!-- <div class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-md text-yellow-100 bg-yellow-700 border border-yellow-700 ">
+            <div slot="avatar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info w-5 h-5 mx-2">
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <line x1="12" y1="16" x2="12" y2="12"></line>
+                    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                </svg>
+            </div>
+            <div class="text-xl font-normal  max-w-full flex-initial">
+               you are required to select at least one organization</div>
+            <div class="flex flex-auto flex-row-reverse">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x cursor-pointer hover:text-yellow-400 rounded-full w-5 h-5 ml-2">
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </div>
+            </div>
+        </div> -->
       <div class="mt-6">
                                   <button
-                                      @click="this.$router.push('/')" class="flex w-1/3 justify-center px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                      @click="choose" class="flex w-1/3 justify-center px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                       Continue
                                   </button>
                               </div>
@@ -730,6 +749,11 @@ function ckChange(ckType){
       Checked--;
    }
    
+}
+
+function choose(){
+   if (Checked) navigateTo('/')
+   else alert("you are required to select at least one organization")
 }
 
 </script>

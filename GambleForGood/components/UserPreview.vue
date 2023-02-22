74af461c743478bc4e506fb9bf1ab301b4cff26f
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2023-02-22 19:13:44 +0100

    huh?

--- GambleForGood/components/UserPreview.vue
+++ GambleForGood/components/UserPreview.vue
@@ -4,7 +4,7 @@
             <div class="flex items-center">
                 <div class="flex-shrink-0 w-10 h-10">
                     <img class="w-full h-full rounded-full"
-                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                        :src= user.imagelink
                         alt="" />
                 </div>
                     <div class="ml-3 cursor-pointer ">

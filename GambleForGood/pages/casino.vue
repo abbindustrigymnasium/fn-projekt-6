6ab2951e2fbb378777a8acfe4fe3ee791ebd79e9
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2023-02-22 20:01:10 +0100

    fix links

--- GambleForGood/pages/casino.vue
+++ GambleForGood/pages/casino.vue
@@ -9,15 +9,19 @@
           </div>
         </div>
         <div class="flex flex-wrap -m-4">
+          
           <div class="xl:w-1/3 md:w-1/2 p-4">
+            <NuxtLink to="/slots">
             <div class="bg-gray-900 p-6 rounded-lg">
               <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6 border-4 border-gray-800" src="https://blog.meccabingo.com/wp-content/uploads/2020/03/Winning-Tips-for-Slots-Games.jpg" alt="Image Size 720x400">
               <!-- <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3> -->
               <h2 class="text-lg text-white font-medium title-font mb-1">Slots</h2>
               <div class="h-1 w-10 bg-indigo-500 rounded"></div>
-              <p class="leading-relaxed text-base"></p>
+              <p class="leading-relaxed text-base"></p>     
             </div>
+          </NuxtLink>
           </div>
+    
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="bg-gray-900 p-6 rounded-lg">
               <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6 border-4 border-gray-800" src="https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/r/roulette-switch/hero">
